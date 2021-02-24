a4640e5cb127745ee9751fb374e90865efe1eb92
SaarthakT <76083818+SaarthakT@users.noreply.github.com>
2021-02-23 19:55:02 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//Create variables here
+var dog
+var happyDog
+var database
+var foodS
+var foodStock
+
+function preload()
+{
+	//load images here
+  loadImage("sprites/dogImg.png");
+  loadImage("sprites/dogImg1.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
